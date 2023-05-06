c31eb7783bf07e4dc87a165c19da51e0bdc7b9ef
RoderCode <Rodersnow@gmail.com>
2023-05-07 00:39:59 +0200

    able to switch from dev or prod build

---
+++ webpack.prod.js
@@ -0,0 +1,59 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  mode: "production",
+  entry: {
+    bundle: path.resolve(__dirname, "src/index.ts"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    clean: true,
+  },
+  devtool: "source-map",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    // port we using
+    port: 3000,
+    // open browser during launch
+    open: true,
+    // enable hot loading
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        // files that match end point
+        test: /\.tsx?$/,
+        // what to with those files
+        use: "ts-loader",
+        // skip ts and tsx files in node_modules folder
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "src/template.html",
+    }),
+  ],
+};
